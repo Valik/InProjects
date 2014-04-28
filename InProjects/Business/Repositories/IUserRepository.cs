fce1b4cfa8aabfdaeef982a7c8fc81b3563565b9
Valik <nevalikne@gmail.com>
2014-04-28 23:37:13 +0400

    [Task1] Added user repository. Started Work with Ninject

---
+++ InProjects/Business/Repositories/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using InProjects.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InProjects.Business.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetCurrentUser();
+        
+        User SearchUserByNickOrEmail(string nickOrEmail);
+
+        User GetUserProfile(int userId);
+
+        bool CreateNewUser(User newUser);
+    }
+}
